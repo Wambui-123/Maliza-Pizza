7c087d265ff62a2b0135f8cea4452d2bcb6ce738
Wambui-123 <yvonne.muthui@student.moringaschool.com>
2022-03-20 08:39:42 +0300

    edited order page

--- order.js
+++ order.js
@@ -22,8 +22,20 @@ function checkButton() {
     else if(document.getElementById('flexRadioDefault6').checked) {  
         document.getElementById("print-crust").innerHTML = document.getElementById("flexRadioDefault6").value         
         //console.log(document.getElementById("flexRadioDefault6").value) 
-    } 
+    }
+    else if(document.getElementById('flexRadioDefault7').checked) {
+        document.getElementById("print-topping").innerHTML = getElementById('print-crust').value + getElementById('print-size').value + 30;
+    }
     else {   
        console.log('You have not choosen any meal')
     }   
-} 
+}
+// var a = getElementById('pizza-size').value;
+// var b = getElementById('pizza-crust').value;
+console.log(document.getElementById('pizza-size').value)
+
+     
+
+// if(document.getElementById('flexRadioDefault1').checked) {
+//     document.getElementById("print-topping").innerHTML = 5 + 6;
+// } 
