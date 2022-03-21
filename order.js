caf07c4f0b3341cd2e55b148a01333fc3ce4183e
Wambui-123 <yvonne.muthui@student.moringaschool.com>
2022-03-21 08:14:16 +0300

    styled order page

--- order.js
+++ order.js
@@ -1,41 +1,97 @@
-function checkButton() {    
-    if(document.getElementById('flexRadioDefault1').checked) { 
-        document.getElementById("print-size").innerHTML = document.getElementById("flexRadioDefault1").value        
-        //console.log(document.getElementById("flexRadioDefault1").value) 
-    }   
-    else if(document.getElementById('flexRadioDefault2').checked) {     
-        document.getElementById("print-size").innerHTML = document.getElementById("flexRadioDefault2").value 
-        //console.log(document.getElementById("flexRadioDefault2").value)         
-    }   
-    else if(document.getElementById('flexRadioDefault3').checked) {  
-        document.getElementById("print-size").innerHTML = document.getElementById("flexRadioDefault3").value        
-        //console.log(document.getElementById("flexRadioDefault3").value)   
-    }   
-    else if(document.getElementById('flexRadioDefault4').checked) {  
-        document.getElementById("print-crust").innerHTML = document.getElementById("flexRadioDefault4").value        
-        //console.log(document.getElementById("flexRadioDefault4").value)  
-    }
-    else if(document.getElementById('flexRadioDefault5').checked) { 
-        document.getElementById("print-crust").innerHTML = document.getElementById("flexRadioDefault5").value          
-        //console.log(document.getElementById("flexRadioDefault5").value) 
-    } 
-    else if(document.getElementById('flexRadioDefault6').checked) {  
-        document.getElementById("print-crust").innerHTML = document.getElementById("flexRadioDefault6").value         
-        //console.log(document.getElementById("flexRadioDefault6").value) 
-    }
-    else if(document.getElementById('flexRadioDefault7').checked) {
-        document.getElementById("print-topping").innerHTML = getElementById('print-crust').value + getElementById('print-size').value + 30;
-    }
-    else {   
-       console.log('You have not choosen any meal')
-    }   
+let totalPrice = 0;
+class Order {
+  constructor(
+    sizePrice,
+    crustPrice,
+    toppingPrice,
+    pizzaFlavour,
+    pizzaSize,
+    pizzaCrust,
+    pizzaTopping
+  ) {
+    this.sizePrice = sizePrice;
+    this.crustPrice = crustPrice;
+    this.toppingPrice = toppingPrice;
+
+    this.pizzaFlavour = pizzaFlavour;
+    this.pizzaSize = pizzaSize;
+    this.pizzaCrust = pizzaCrust;
+    this.pizzaTopping = pizzaTopping;
+  }
+
+  addOrder() {
+    var total =
+    parseInt(this.crustPrice) +
+    parseInt(this.sizePrice) +
+    parseInt(this.toppingPrice);
+
+    const order = `
+    <div class="order">
+        <div class="check"> ✔️ </div>
+
+        <div style="flex: 1; font-size: 16px;">
+            <p>${this.pizzaFlavour}</p>
+            <p>Size - ${this.pizzaSize}</p>
+            <p>Toppings - ${this.pizzaTopping}</p>
+            <p>Crust - ${this.pizzaCrust}</p>
+            <p>Total - ${total}</p>
+        <div>
+    </div>`;
+
+    $("#order-container").append(order);
+    
+    totalPrice = totalPrice + total;
+    const totalElem = document.getElementById("total");
+    totalElem.innerHTML = totalPrice;
+  }
 }
-// var a = getElementById('pizza-size').value;
-// var b = getElementById('pizza-crust').value;
-console.log(document.getElementById('pizza-size').value)
 
-     
+function handleOrder() {
+  var sizeOfPizza = $("#pizza-size option:selected").text();
+  var crustOfPizza = $("#pizza-crust option:selected").text();
+  var toppingOfPizza = $("#pizza-topping option:selected").text();
+  var flavourOfPizza = $("#pizza-flavour option:selected").text();
 
-// if(document.getElementById('flexRadioDefault1').checked) {
-//     document.getElementById("print-topping").innerHTML = 5 + 6;
-// } 
+  var sizePrice = $("#pizza-size").val();
+  var crustPrice = $("#pizza-crust").val();
+  var toppingPrice = $("#pizza-topping").val();
+
+  const newOrder = new Order(
+    sizePrice,
+    crustPrice,
+    toppingPrice,
+    flavourOfPizza,
+    sizeOfPizza,
+    crustOfPizza,
+    toppingOfPizza
+  );
+
+  newOrder.addOrder();
+}
+
+$("#pizza-flavour").change(function () {
+  $("#flavour").html($(this).val());
+});
+$("#pizza-size").change(function () {
+  $("#size").html($(this).val());
+});
+$("#pizza-crust").change(function () {
+  $("#crust").html($(this).val());
+});
+$("#pizza-topping").change(function () {
+  $("#topping").html($(this).val());
+});
+$("#btn").click(function () {
+  $("#toggle").toggle();
+});
+$("#submit-address").click(function () {
+  $("#address-val").html(`to ${$("#address").val()}`);
+  $(".para").toggle();
+  $(".delivery-section").hide();
+})
+$("#yes").click(function () {
+  $(".delivery-section").toggle();
+});
+$("#no").click(function () {
+  $(".para").toggle();
+});
